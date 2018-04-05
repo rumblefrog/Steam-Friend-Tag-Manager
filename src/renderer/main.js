7f063510737b3456e498b86e8df3e683345395a0
RumbleFrog <xxlosttechxx@gmail.com>
2018-04-05 11:31:06 -0400

    :lipstick: Bulma & Buefy

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,16 +1,27 @@
-import Vue from 'vue';
+import Vue from "vue";
+import Buefy from "buefy";
+import 'buefy/lib/buefy.css';
 
-import App from './App';
-import router from './router';
-import store from './store';
+// import fontawesome from "@fortawesome/fontawesome";
+// import solid from "@fortawesome/fontawesome-free-solid";
+// import regular from "@fortawesome/fontawesome-free-regular";
+// import brands from "@fortawesome/fontawesome-free-brands";
 
-if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
+import App from "./App";
+import router from "./router";
+import store from "./store";
+
+if (!process.env.IS_WEB) Vue.use(require("vue-electron"));
 Vue.config.productionTip = false;
+
+Vue.use(Buefy, {
+  defaultIconPack: "fas"
+});
 
 /* eslint-disable no-new */
 new Vue({
   components: { App },
   router,
   store,
-  template: '<App/>',
-}).$mount('#app');
+  template: "<App/>"
+}).$mount("#app");
