b79c983015e0aff908c17c76f23fb75d8e095f14
RumbleFrog <xxlosttechxx@gmail.com>
2018-04-21 12:35:52 -0400

    :zap: Decrease Timeout

--- src/renderer/components/Dashboard.vue
+++ src/renderer/components/Dashboard.vue
@@ -218,7 +218,7 @@ export default {
 
       this.timeout = setTimeout(() => {
         this.processQueue();
-      }, 2000);
+      }, 1000);
     },
     processQueue() {
       this.loading = true;
